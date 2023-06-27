8ec47e974321672477b1d343a036336bd75e79dd
Spider1357 <Spider_1357@hotmail.com>
2023-06-27 23:34:37 +1000

    Updated common functions for readability

--- common.py
+++ common.py
@@ -1,11 +1,11 @@
 import hashlib
 
-def hash(s):
-    if s == None:
+def hash(string: str) -> str:
+    if string == None:
         return ""
-    return hashlib.sha256(clean(s).encode("UTF-8")).hexdigest()
+    return hashlib.sha256(clean(string).encode("UTF-8")).hexdigest()
 
-def clean(s):
-    if s == None:
+def clean(string: str) -> str:
+    if string == None:
         return ""
-    return s.replace(" ", "").upper()
+    return string.replace(" ", "").upper()
