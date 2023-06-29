c3e051d223ba9f3bb29061ae06a786cf9156507a
Spider1357 <Spider_1357@hotmail.com>
2023-06-29 11:31:29 +1000

    Converted clue generation function into a reusable object

--- clueGeneration.py
+++ clueGeneration.py
@@ -1,34 +1,30 @@
 import pygame
 
-lineSpace = 2 # Spacing between lines
-scalar = 3 # Background image upscalar
-fontSize = 48 # Font size
-fontColour =  (255, 255, 0) # Font colour
-outputFileName = "generatedClue.png"
+class ClueGenerator:
+    def __init__(self, bg_image_path: str, output_path: str):
+        # Start up pygame
+        pygame.init()
+        _ = pygame.display.set_mode((1, 1), pygame.NOFRAME)
+        self.bg_image = pygame.image.load(bg_image_path).convert_alpha()
+        self.bg_width, self.bg_height = self.bg_image.get_size()
 
-def clueFromString(clueText: str) -> None:
-    clueFromLines(clueText.split("\n"))
+        self.output_path = output_path
+        self.font_colour = (255, 255, 0)
 
-def clueFromLines(clueText: list[str]) -> None:
-    pygame.init()
-    _ = pygame.display.set_mode((1, 1), pygame.NOFRAME)
-    bg = pygame.image.load("blankClue.png").convert_alpha()
-    bg = pygame.transform.scale(bg, (bg.get_width() * scalar, bg.get_height() * scalar))
-    font = pygame.font.SysFont("Runescape Chat '07 Regular", fontSize)
+    def generate_clue(self, text_input: list[str], line_space: int = 2, scalar: int = 3, font_size: int = 48):
+        bg_image = pygame.transform.scale(self.bg_image, (self.bg_width * scalar, self.bg_height * scalar))
+        font = pygame.font.SysFont("Runescape Chat '07 Regular", font_size)
 
-    srf = pygame.Surface(bg.get_size(), pygame.SRCALPHA)
-    srf.blit(bg, (0, 0))
+        rendered_text = [font.render(text, False, self.font_colour) for text in text_input]
+        text_height = rendered_text[0].get_height()
+        total_height = (len(rendered_text) * (line_space + text_height)) - line_space
 
-    renderedText = [font.render(text, False, fontColour) for text in clueText]
-    textHeight = renderedText[0].get_height()
-    totalHeight = (len(renderedText) * (lineSpace + textHeight)) - lineSpace
+        top_offset = (bg_image.get_height() - total_height) // 2
+        x_pos = bg_image.get_width() // 2
+        for idx, text in enumerate(rendered_text):
+            y_pos = top_offset + (idx * (text_height + line_space))
+            rct = text.get_rect()
+            rct.midtop = (x_pos, y_pos)
+            bg_image.blit(text, rct)
 
-    topOffset = (bg.get_height() - totalHeight) // 2
-    xPos = bg.get_width() // 2
-    for idx, text in enumerate(renderedText):
-        yPos = topOffset + (idx * (textHeight + lineSpace))
-        rct = text.get_rect()
-        rct.midtop = (xPos, yPos)
-        srf.blit(text, rct)
-
-    pygame.image.save(srf, outputFileName)
+        pygame.image.save(bg_image, self.output_path)
