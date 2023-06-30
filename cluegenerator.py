08a6375dd7026606347905230ddf76574c1bbe2f
Spider1357 <Spider_1357@hotmail.com>
2023-06-30 23:15:58 +1000

    Generated clue is no longer saved to disk at all generate_clue now returns bytes object instead of saving Saved paths are now outside of ClueGenerator, which it now accepts as inputs

--- cluegenerator.py
+++ cluegenerator.py
@@ -1,14 +1,15 @@
+import io
 import contextlib
 from pathlib import Path
 with contextlib.redirect_stdout(None):
     import pygame
 
 class ClueGenerator:
-    def __init__(self, bg_image_path: str, font_path: str, output_path: str):
+    def __init__(self, bg_image_path: Path, font_path: Path, generated_clue_name: str):
         # Relevant file paths
-        self.bg_image_path = Path(bg_image_path)
-        self.font_path = Path(font_path)
-        self.output_path = Path(output_path)
+        self.bg_image_path = bg_image_path
+        self.font_path = font_path
+        self.generated_clue_name = generated_clue_name
 
         # Start up pygame
         pygame.init()
@@ -19,7 +20,7 @@ class ClueGenerator:
         self.bg_width, self.bg_height = self.bg_image.get_size()
         self.font_colour = (255, 255, 0)
 
-    def generate_clue(self, text_input: list[str], line_space: int = 2, scalar: float = 1.0, font_size: int = 48) -> Path:
+    def generate_clue(self, text_input: list[str], line_space: int = 2, scalar: float = 1.0, font_size: int = 48) -> io.BytesIO:
         # Scale normal image to 3x for normal appearance with font
         bg_image = pygame.transform.scale(self.bg_image, (int(self.bg_width * 3 * scalar), (self.bg_height * 3 * scalar)))
         font = pygame.font.Font(self.font_path, font_size)
@@ -36,5 +37,7 @@ class ClueGenerator:
             rct.midtop = (x_pos, y_pos)
             bg_image.blit(text, rct)
 
-        pygame.image.save(bg_image, self.output_path)
-        return self.output_path
+        img = io.BytesIO()
+        pygame.image.save(bg_image, img, self.generated_clue_name)
+        img.seek(0)
+        return img
